50ad2e973e11ae66f7ac4901a4740e3dd20eabc8
salman18 <salmanaleemkhan786@gmail.com>
2020-08-12 21:27:23 +0530

    http header exercise

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/',(req,res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.get('/demo',(req,res) => {
+  res.set('X-full-stack','4life');
+  res.status(418);
+  res.send('I prefer coffee')
+})
+
+app.listen(port,() => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
